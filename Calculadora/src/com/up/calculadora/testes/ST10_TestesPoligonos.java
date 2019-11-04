6b8aa40129c019a26a86b17d4f70b4c25166e779
Jociel Barbosa <jocieltb@gmail.com>
2019-11-04 14:14:23 -0300

    Calculadora

---
+++ Calculadora/src/com/up/calculadora/testes/ST10_TestesPoligonos.java
@@ -0,0 +1,16 @@
+package com.up.calculadora.testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.up.calculadora.Calculadora;
+
+class ST10_TestesPoligonos {
+
+	@Test
+	void CT01() {
+		assertEquals(1, Calculadora.doisValores(1, 1));
+	}
+
+}
